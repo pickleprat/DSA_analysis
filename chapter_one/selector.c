97b69dd1f3199e32aaf0306a2062f427026ee6c4
pickleprat <prat16here@gmail.com>
2024-09-28 22:46:06 +0530

    fixed the bug with a bit cleaner and less confusing code.

--- chapter_one/selector.c
+++ chapter_one/selector.c
@@ -3,60 +3,79 @@
 #include <time.h> 
 #include "random_vector.c"
 
+// k = 50 
 int kth_smallest_sorting(int* nums, int size, int k) {
 	quick_sort(nums, 0, size-1, size); 
 	return nums[size - k]; 
 } 
 
+void insert_sorted(int number, int* store, int size) {
+	int i = 0; 
+	while( store[i] < number && i < size ) { i++; } 
+	i--; 
+	for(int j = 0; j < i; j++) {
+		store[j] = store[j+1]; 
+	} 
+	
+	store[i] = number; 
+} 
+
+
 int kth_smallest_selection(int* nums, int size, int k) {
-	int* store = calloc(sizeof(int), k); 
-	for(int i = 0; i < k; i++) {
+	int* store = (int* ) calloc(sizeof(int), k); 
+
+	for(int i = 0; i < k; i++) { 
 		store[i] = nums[i]; 
-	}; 
+	} 
 
 	quick_sort(store, 0, k-1, k); 
-
+	
 	for(int i = k; i < size; i++) {
-		int j = 0; 
-		if(nums[i] > store[j]) {
-			while(store[j] < nums[i] && j < k) {
-				j++; 
-			}
-
-			for(int m = 1; m < j; m++) {
-				store[m-1] = store[m]; 
-			} 
-
-			store[j - 1] = nums[i]; 
+		if(nums[i] > store[0]) {
+			insert_sorted(nums[i], store, k); 
 		} 
+
 	} 
 
 	return store[0]; 
 } 
 
-
 int main(void) {
-	int sizes[10] = {100, 200, 500, 1000, 2000, 10000, 50000, 100000, 200000, 250000};  
+	int sizes[10] = {100, 200, 300, 1000, 2000, 10000, 50000, 100000, 600000, 700000};  
 	int* vector; 
 	double begin, end; 
+	int error = 0; 
 	for(int i = 0; i < 10; i++) {
+		
 		vector = rvector(sizes[i], 0, 1000000); 
+		printf("================================================================\n"); 
+		printf("Test %d: element count: %d\n", i + 1, sizes[i]); 
+
+
+		begin = clock(); 
+		int selection_output = kth_smallest_selection(vector, sizes[i], sizes[i] / 2); 
+		end = clock(); 
+
+		printf("The time elapsed for selection : %lf s\n",(double) (end - begin) / CLOCKS_PER_SEC ); 
+
+
 		begin = clock(); 
 		int sorting_output = kth_smallest_sorting(vector, sizes[i], sizes[i]/2); 
 		end = clock(); 
 
-		printf("The time elapsed for sorting : %lf\n", (double) (end - begin) / CLOCKS_PER_SEC); 
+		printf("The time elapsed for sorting : %lf s\n", (double) (end - begin) / CLOCKS_PER_SEC); 
 		
-		begin = clock(); 
-		int selection_output = kth_smallest_selection(vector, 0, sizes[i] / 2); 
-		end = clock(); 
-
-		printf("The time elapsed for selection : %lf\n",(double) (end - begin) / CLOCKS_PER_SEC ); 
 		if(sorting_output != selection_output) {
-			printf("The sorting output %d is not equal to the selection output %d\n", sorting_output, selection_output);  
-		} else {
-			printf("The tests ran successfully! The results are above\n"); 
-		} 
-
+			error = 1; 
+			printf("ERROR: The sorting output %d is not equal to the selection output %d\n", sorting_output, selection_output);  
+		}
+		printf("=====================================================================\n");
 	}
+
+	if(error == 0) {
+		printf("The test succeeded!\n"); 
+	} 
+		
+
+	
 } 
